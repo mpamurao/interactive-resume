ff21ac8a1faf0df12f4b7e883b99fd4ac14a16c9
Marie Amurao <mamurao@g.clemson.edu>
2021-05-26 18:25:18 -0400

    wrote tests for vendingMachine

--- Projects List/Vending Machine Lab/vendingMachine.js
+++ Projects List/Vending Machine Lab/vendingMachine.js
@@ -1,5 +1,5 @@
 // object called vendingMachine
-var vendingMachine = {
+const vendingMachine = {
     // snack array to include objects with name and price
     snack: [
         {name:"oreos", price: 0.75},
@@ -9,18 +9,14 @@ var vendingMachine = {
     // method to print the item in snack array
     vend: (index) => {
             // check for valid input
-            if (index < 0){
-                return `Invalid input of ${index}. Please use a number greater than or equal to zero to request a snack.`;
+            if (index < 0 || index > vendingMachine.snack.length - 1){
+                return `Invalid input of ${index}. Please use a number within the range of snacks to request a snack.`;
             }
 
-            console.log(vendingMachine.snack[index]);
+            // console.log(vendingMachine.snack[index]);
             return `You requested to buy ${vendingMachine.snack[index].name}. The price is $${vendingMachine.snack[index].price}. Please enter this amount in dollars or coins.`
     }
 }
 
-console.log(vendingMachine.vend(0));
-console.log(vendingMachine.vend(1));
-console.log(vendingMachine.vend(2));
-console.log(vendingMachine.vend(-1));
-
-export default vendingMachine;
+// module.exports - node.js keyword to export file
+module.exports = vendingMachine;
--- Projects List/Vending Machine Lab/vendingMachine.test.js
+++ Projects List/Vending Machine Lab/vendingMachine.test.js
@@ -1,4 +1,23 @@
-import vendingMachine from './vendingMachine';
-
 // test cases for vendingMachine.vend()
+// install jest to run test cases
+// https://www.freecodecamp.org/news/how-to-start-unit-testing-javascript/
 
+// file to be tested
+const vendingMachine = require("./vendingMachine");
+
+test("Returns response for valid input", () => {
+    // if valid input of an index in the vendingMachine.snack array
+    vendingMachine.snack.forEach((snack, index) => {
+        expect(vendingMachine.vend(index))
+            .toBe(`You requested to buy ${snack.name}. The price is $${snack.price}. Please enter this amount in dollars or coins.`);
+    });
+});
+
+test("Returns response for invalid inputs", () => {
+    // if input is not an index in the snack array
+    let invalidInputs = [-1, -2, -3, vendingMachine.snack.length + 1]
+    invalidInputs.forEach(input => {
+        expect(vendingMachine.vend(input))
+            .toBe(`Invalid input of ${input}. Please use a number within the range of snacks to request a snack.`);
+    });
+});
